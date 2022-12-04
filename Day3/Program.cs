ba83790d306e350d090b4fe511f8edb412be859d
timmoth <t.jones@timmoth.com>
2022-12-04 11:57:22 +0000

    Finished day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,35 @@
+﻿
+var input = File.ReadAllLines("./day3.csv");
+var score = Day3.CalculateTotalPriority(input);
+
+Console.WriteLine(score);
+Console.Read();
+
+public static class Day3
+{
+    public static int CalculateTotalPriority(string[] input)
+    {
+        int totalPriority = 0;
+
+        Span<string> inputAsSpan = input;
+        for (int i = 0; i < inputAsSpan.Length; i++)
+        {
+            ReadOnlySpan<char> line = inputAsSpan[i];
+            var midPoint = line.Length / 2;
+            var compartmentOne = line[..midPoint];
+            var compartmentTwo = line.Slice(midPoint, midPoint);
+
+            for(int j = 0; j < compartmentTwo.Length; j++)
+            {
+                var item = compartmentTwo[j];
+                if (compartmentOne.Contains(item))
+                {
+                    totalPriority += item - (char.IsUpper(item) ? 38 : 96);
+                    break;
+                }
+            }
+        }
+
+        return totalPriority;
+    }
+}
---
+++ Tests/Day3Tests.cs
@@ -0,0 +1,23 @@
+namespace Tests
+{
+    public class Day3Tests
+    {
+        [Theory]
+        [InlineData(1, "aa")]
+        [InlineData(27, "AA")]
+        [InlineData(2, "abbc")]
+        [InlineData(52, "aba", "ZZ")]
+        [InlineData(157, "vJrwpWtwJgWrhcsFMMfFFhFp", "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "PmmdzqPrVvPwwTWBwg", "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", "ttgJtRGJQctTZtZT", "CrZsJsPPZsGzwwsLwLmpwMDw")]
+        public void CalculateTotalPriorityReturnsTotalPriority(int expectedValue, params string[] input)
+        {
+            //Given 
+            var inputLines = input.ToArray();
+
+            //When
+            var output = Day3.CalculateTotalPriority(inputLines);
+
+            //Then
+            Assert.Equal(expectedValue, output);
+        }
+    }
+}
