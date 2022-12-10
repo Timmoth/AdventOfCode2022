a21316f06a03674fa32827112209ce5f17bdb16b
timmoth <t.jones@timmoth.com>
2022-12-10 15:22:34 +0000

    Further optimised day10

--- Solutions/Day10.cs
+++ Solutions/Day10.cs
@@ -40,26 +40,23 @@ public static class Day10
         var cycleCount = 0;
         var register = 1;
         var output = new StringBuilder();
-        var outputLineBuilder = new StringBuilder();
         do
         {
             var pixelIndex = cycleCount % 40;
-            outputLineBuilder.Append(pixelIndex >= register - 1 && pixelIndex <= register + 1 ? '#' : '.');
+            output.Append(pixelIndex >= register - 1 && pixelIndex <= register + 1 ? '#' : '.');
             if (++cycleCount % 40 == 0)
             {
-                output.AppendLine(outputLineBuilder.ToString());
-                outputLineBuilder.Clear();
+                output.AppendLine();
             }
 
             var line = input[instructionIndex].AsSpan();
             if (line[0] == 'a')
             {
                 pixelIndex = cycleCount % 40;
-                outputLineBuilder.Append(pixelIndex >= register - 1 && pixelIndex <= register + 1 ? '#' : '.');
+                output.Append(pixelIndex >= register - 1 && pixelIndex <= register + 1 ? '#' : '.');
                 if (++cycleCount % 40 == 0)
                 {
-                    output.AppendLine(outputLineBuilder.ToString());
-                    outputLineBuilder.Clear();
+                    output.AppendLine();
                 }
 
                 register += int.Parse(line[4..]);
